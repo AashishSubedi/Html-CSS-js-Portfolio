5881f0aba5c53aac168df3b5479cb42c7e5e9b73
Aashish Subedi <ashishsubedi08@gmail.com>
2024-12-25 03:26:08 -0600

    Portfolio

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open");
 }
+
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
